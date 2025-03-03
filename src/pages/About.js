b5cc28a1981131b07966e1dc9f8e8970a7bc7fb1
FrankNeba <ngwafranklin29@gmail.com>
2025-03-03 20:38:28 +0100

    Projects view adjusted for desktop

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,14 +10,25 @@ function About() {
         <div className="about-me">
           <h2>About Me</h2>
           <p>
-            I am a Full-Stack Developer with over 3 years of experience,
-            specializing in HTML, CSS, Bootstrap, JavaScript, React.js, Python,
-            and Django. I also have knowledge of C and C++, along with expertise
-            in version control systems like Git and GitHub. Throughout my
-            career, I have worked on numerous projects, delivering user-friendly
-            and highly navigable websites and applications for satisfied
-            clients. I thrive on collaborating with others to develop innovative
-            solutions and continuously expand my skill set.
+            I am a Full-Stack Developer with over 3 years of experience in
+            designing and developing scalable, user-friendly web and mobile
+            applications. My expertise includes HTML, CSS, Bootstrap,
+            JavaScript, React.js, Python, and Django. Additionally, I have a
+            solid understanding of C and C++, as well as experience with version
+            control systems like Git and GitHub. I have worked on a wide range
+            of projects for different clients, ensuring high-quality,
+            responsive, and easily navigable applications. My ability to
+            collaborate effectively with teams and individuals has allowed me to
+            create seamless digital experiences that meet client expectations. I
+            have also worked as a Full-Stack Developer at ZiloTech, where I
+            contributed to the development and optimization of web applications,
+            implementing modern technologies to enhance functionality and
+            performance. My time at ZiloTech helped me refine my problem-solving
+            skills and deepen my understanding of building scalable
+            applications. I am passionate about continuous learning and always
+            seek opportunities to expand my skill set while working on
+            innovative projects. I look forward to collaborating with others to
+            bring ideas to life through technology.
           </p>
         </div>
       </div>
